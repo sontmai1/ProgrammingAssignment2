f61a01f5ba08b132d5b1aca6644ae2578d94ede7
Son Mai <son.thanh.mai@gmail.com>
2023-03-27 22:58:47 -0400

    Update test-2.R

--- test-2.R
+++ test-2.R
@@ -1,17 +1,17 @@
 source("cachematrix.R")
 
 
-# Create a special matrix object
+# Create special matrix object
 myMatrix <- makeCacheMatrix(matrix(1:4, nrow = 2))
 
-# Compute the inverse (which should not be cached yet)
+# Compute the inverse (not be cached yet)
 cachesolve(myMatrix)
 
-# Compute the inverse again (which should be retrieved from cache)
+# Compute the inverse again (retrieved from cache)
 cachesolve(myMatrix)
 
 # Change the matrix
 myMatrix$setMatrix(matrix(c(3, 2, 2, 1), nrow = 2))
 
-# Compute the inverse again (which should be recalculated)
+# Compute the inverse again (recalculated)
 cachesolve(myMatrix)
